94772eace0a25a2a5a2930e4e8a051bd53a53e73
lohare21 <lohare@iu.edu>
2021-12-11 14:01:53 -0500

    Initial Commit

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,6 +1,8 @@
 extends KinematicBody
 
-onready var Camera = get_node("/root/Game/Player")
+onready var Camera = get_node("/root/Game/Player/Pivot/Camera")
+onready var Pivot = get_node("/root/Game/Player/Pivot")
+onready var target = get_node("/root/Game/Target_Container/Target")
 
 var velocity = Vector3()
 var gravity = -9.8
@@ -29,9 +31,15 @@ func _physics_process(_delta):
 	$AnimationTree.set("parameters/Idle_Run/blend_amount", current_speed/max_speed)
 	velocity = move_and_slide(velocity, Vector3.UP, true)
 	
+	
+	if Input.is_action_just_pressed("shoot") and target != null and target.is_in_group("target"):
+		target.die()
+	
 func _input(event):
 	if event is InputEventMouseMotion:
-		rotate_y(-event.relative.x * mouse_sensitivity) 
+		rotate_y(-event.relative.x * mouse_sensitivity)
+		Pivot.rotate_x(event.relative.y * mouse_sensitivity)
+		Pivot.rotation_degrees.x = clamp(Pivot.rotation_degrees.x, -30, 15)
 	
 func get_input():
 	var input_dir = Vector3()
